72677ded76c1abc087bea93861c2109847b1ec10
Tara <taraplatt88@gmail.com>
2024-03-29 11:05:32 -0700

    init

---
+++ app/models/Notes.js
@@ -0,0 +1,13 @@
+import { generateId } from "../utils/GenerateId.js"
+
+export class Jumble {
+    constructor(data) {
+        this.id = generateId()
+        this.name = data.name
+        this.body = data.body
+        // to best keep track of the fastest times you might want these properties too! They would start null cause no one has completed these yet.
+        this.fastestTime = Infinity
+        this.startTime = null
+        this.endTime = null
+    }
+}
